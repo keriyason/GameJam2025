57b58594ca4cd86021cf28122a7ac1176c847c50
keriyason <keriyason@lcad.edu>
2025-11-08 00:27:02 -0800

    Player Movement Script
    updated player movement script
    imported Pencil case

--- GameJam2025/Assets/Scripts/Player Movement.cs
+++ GameJam2025/Assets/Scripts/Player Movement.cs
@@ -5,24 +5,30 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
-    public float rotationSpeed = 100f;
     public Rigidbody rb;
+
+
     void FixedUpdate()
     {
-        // Player 1 Input
-        float player1Horizontal = Input.GetAxis("Player1Horizontal");
-        float player1Vertical = Input.GetAxis("Player1Vertical");
 
-        // Player 2 Input
-        float player2Horizontal = Input.GetAxis("Player2Horizontal");
-        float player2Vertical = Input.GetAxis("Player2Vertical");
+        //get key W A S D
 
-        // Combine or process inputs
-        Vector3 combinedMovement = new Vector3(player1Horizontal + player2Horizontal, 0, player1Vertical + player2Vertical);
-        combinedMovement *= speed;
-        //transform.Translate(combinedMovement * speed * Time.deltaTime, Space.World);
-        //rb.AddForce(new Vector3 (player1Horizontal, 0, player1Vertical));
-        rb.AddForce(combinedMovement);
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(0, 3, -3);
+            rb.AddForce(transform.right * speed, ForceMode.Impulse);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Rotate(0, -3, -3);
+            rb.AddForce(-transform.right * speed, ForceMode.Impulse);
+        }
+
+
 
     }
+
+
+
 }
