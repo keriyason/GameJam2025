c681f67368a9fd4299a04fda15e26556149e48a9
keriyason <keriyason@lcad.edu>
2025-11-07 21:57:11 -0800

    Player Movement
    feat changes to player movement
    feat joint

--- GameJam2025/Assets/Scripts/Player Movement.cs
+++ GameJam2025/Assets/Scripts/Player Movement.cs
@@ -6,8 +6,8 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f;
     public float rotationSpeed = 100f;
-
-    void Update()
+    public Rigidbody rb;
+    void FixedUpdate()
     {
         // Player 1 Input
         float player1Horizontal = Input.GetAxis("Player1Horizontal");
@@ -20,6 +20,7 @@ public class PlayerMovement : MonoBehaviour
         // Combine or process inputs
         Vector3 combinedMovement = new Vector3(player1Horizontal + player2Horizontal, 0, player1Vertical + player2Vertical);
         transform.Translate(combinedMovement * speed * Time.deltaTime, Space.World);
+        rb.AddForce(new Vector3 (player1Horizontal, 0, player1Vertical));
 
     }
 }
