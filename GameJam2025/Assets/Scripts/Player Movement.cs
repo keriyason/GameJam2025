85c55ae5a42ff2af94befcccb06edd83c530a4aa
keriyason <keriyason@lcad.edu>
2025-11-08 19:41:23 -0800

    updated player script
    player script updated

--- GameJam2025/Assets/Scripts/Player Movement.cs
+++ GameJam2025/Assets/Scripts/Player Movement.cs
@@ -3,33 +3,46 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
+
 {
     public float speed = 5f;
-    public Rigidbody rb;
+public Rigidbody rb;
 
-    void FixedUpdate()
-    {
-        // Handle movement input one direction at a time
-        if (Input.GetKey(KeyCode.D))
+private KeyCode lastKeyPressed;
+
+void Update()
+{
+    // Track the most recent key press
+    if (Input.GetKeyDown(KeyCode.D)) lastKeyPressed = KeyCode.D;
+    else if (Input.GetKeyDown(KeyCode.A)) lastKeyPressed = KeyCode.A;
+    else if (Input.GetKeyDown(KeyCode.RightArrow)) lastKeyPressed = KeyCode.RightArrow;
+    else if (Input.GetKeyDown(KeyCode.LeftArrow)) lastKeyPressed = KeyCode.LeftArrow;
+}
+
+void FixedUpdate()
+{
+        if (lastKeyPressed==KeyCode.D && Input.GetKey(KeyCode.D))
         {
             transform.Rotate(0, 3, -3);
             rb.AddForce(transform.right * speed, ForceMode.Impulse);
         }
-        else if (Input.GetKey(KeyCode.A))
+        else if (lastKeyPressed == KeyCode.A && Input.GetKey(KeyCode.A))
         {
             transform.Rotate(0, -3, 3);
             rb.AddForce(-transform.right * speed, ForceMode.Impulse);
         }
-        else if (Input.GetKey(KeyCode.RightArrow))
+        else if (lastKeyPressed == KeyCode.RightArrow && Input.GetKey(KeyCode.RightArrow))
         {
             transform.Rotate(0, -3, -3);
             rb.AddForce(transform.right * speed, ForceMode.Impulse);
         }
-        else if (Input.GetKey(KeyCode.LeftArrow))
+        else if (lastKeyPressed == KeyCode.LeftArrow && Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Rotate(0, 3, 3);
             rb.AddForce(-transform.right * speed, ForceMode.Impulse);
         }
     }
 }
+
+
 
