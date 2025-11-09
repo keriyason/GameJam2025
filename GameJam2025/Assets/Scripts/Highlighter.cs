91ce73c203ee5c3f11abbe0793e475ad69854350
keriyason <keriyason@lcad.edu>
2025-11-09 12:34:55 -0800

    hightlighter
    hightligher script

--- GameJam2025/Assets/Scripts/Highlighter.cs
+++ GameJam2025/Assets/Scripts/Highlighter.cs
@@ -1,13 +1,19 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Highlighter : MonoBehaviour
 {
+    
     public GameObject highlighterPrefab;
     public Transform spawnPoint;
     public float spawnInterval;
 
+    private int spawnCount = 0;
+    private const int maxSpawns = 3;
+
+    /*
     // Start is called before the first frame update
     void Start()
     {
@@ -20,5 +26,22 @@ public class Highlighter : MonoBehaviour
         yield return new WaitForSeconds(spawnInterval);
         Instantiate(highlighterPrefab, spawnPoint.position, Quaternion.identity);
     }
+    */
 
+    private void Start()
+    {
+        InvokeRepeating("SpawnHighlighter", 30f, 3);
+    }
+
+    void SpawnHighlighter()
+    {
+        if (spawnCount >= maxSpawns)
+        {
+            CancelInvoke("SpawnHighlighter");
+            return;
+        }
+
+        Instantiate(highlighterPrefab, spawnPoint.position, Quaternion.identity);
+        
+    }
 }
