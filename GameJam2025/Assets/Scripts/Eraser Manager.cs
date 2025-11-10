a3b3425b53b5e155ab888ae57beb3c98e43e2da7
keriyason <keriyason@lcad.edu>
2025-11-09 16:54:27 -0800

    52
    push edits

--- GameJam2025/Assets/Scripts/Eraser Manager.cs
+++ GameJam2025/Assets/Scripts/Eraser Manager.cs
@@ -8,7 +8,7 @@ public class EraserManager : MonoBehaviour
 {
     public Slider progressSlider; //Slider Attachment
     public int totalErasables = 10; //Total of Erased
-    public int winThreshold = 46;
+    public int winThreshold = 52;
     private int erasedCount = 0;
     public string winSceneName = "WinScene"; //Win Scene Transition
 
